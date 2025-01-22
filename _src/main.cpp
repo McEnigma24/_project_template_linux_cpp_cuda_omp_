e25776e1775bd8c27c453d094d890d8e2c5116b0
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-01-22 23:38:13 +0100

    quick

--- _src/main.cpp
+++ _src/main.cpp
@@ -1,31 +1,24 @@
 #include "__preprocessor__.h"
 #include "time_stamp.h"
 
-namespace TESTING
-{
+namespace TESTING {}
 
-}
-
-class Test
-{
-    Test()
-    {
-        cout << "Test creation" << endl;
-        cout << "next line\n";
-    }
+class Test {
+  Test() {
+    cout << "Test creation" << endl;
+    cout << "next line\n";
+  }
 };
 
 // COmment check
 void func() { cout << "test" << endl; }
 
-int main(int argc, char *argv[])
-{
-    time_stamp("It just works");
+int main(int argc, char *argv[]) {
+  time_stamp("It just works");
 
-    if (true)
-    {
-        cout << "test" << endl;
-    }
+  if (true) {
+    cout << "test" << endl;
+  }
 
-    return 0;
+  return 0;
 }
