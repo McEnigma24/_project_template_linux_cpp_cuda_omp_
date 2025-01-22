9e0b2a030101e866d9228f7bd3ec76ca2b253067
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-01-22 23:48:05 +0100

    quick

--- _src/_format_tester_.cpp
+++ _src/_format_tester_.cpp
@@ -33,7 +33,11 @@ enum TestEnum
     Value2
 };
 
-void freeFunction() { std::cout << "Free function" << std::endl; }
+void freeFunction()
+{
+    std::cout << "Free 1 function" << std::endl;
+    std::cout << "Free 2 function" << std::endl;
+}
 
 int main_test()
 {
