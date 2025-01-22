daf07eb8eccd4141682bc66ee289baa6b2ab91c0
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-01-22 23:38:31 +0100

    quick

--- _src/main.cpp
+++ _src/main.cpp
@@ -4,21 +4,21 @@
 namespace TESTING {}
 
 class Test {
-  Test() {
-    cout << "Test creation" << endl;
-    cout << "next line\n";
-  }
+    Test() {
+        cout << "Test creation" << endl;
+        cout << "next line\n";
+    }
 };
 
 // COmment check
 void func() { cout << "test" << endl; }
 
 int main(int argc, char *argv[]) {
-  time_stamp("It just works");
+    time_stamp("It just works");
 
-  if (true) {
-    cout << "test" << endl;
-  }
+    if (true) {
+        cout << "test" << endl;
+    }
 
-  return 0;
+    return 0;
 }
