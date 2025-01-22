61cde1fa241e30727c40b8e18d20083fd59a18e9
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-01-22 23:26:33 +0100

    quick

--- push.sh
+++ push.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-install_hook()
-{
-    DIR_SCRIPTS="scripts"
-    PATH_MY_HOOK="$DIR_SCRIPTS/formating_hook.sh"
-    PATH_GIT_HOOK=".git/hooks/pre-commit"
-
-    cp $PATH_MY_HOOK $PATH_GIT_HOOK
-    chmod +x $PATH_GIT_HOOK
-}
-
-install_hook
-
 git add .; git commit -m "quick"; git push
 
 # git commit -am "quick"; git push # ! does not include new files !
