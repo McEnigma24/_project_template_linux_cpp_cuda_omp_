d2f6fc42c62a499ab24833f6cf07b61fe3e8e9c2
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-01-22 23:08:06 +0100

    quick

--- _src/main.cpp
+++ _src/main.cpp
@@ -3,23 +3,23 @@
 
 class Test
 {
-    Test()
-    {
-        cout << "Test creation" << endl;
-        cout << "next line\n";
-    }
+ Test()
+ {
+  cout << "Test creation" << endl;
+  cout << "next line\n";
+ }
 };
 
 void func() { cout << "test" << endl; }
 
 int main(int argc, char *argv[])
 {
-    time_stamp("It just works");
+ time_stamp("It just works");
 
-    if (true)
-        {
-            cout << "test" << endl;
-        }
+ if (true)
+  {
+   cout << "test" << endl;
+  }
 
-    return 0;
+ return 0;
 }
