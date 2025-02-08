1e1e82e0af58850fb587a31e8779f581a93cc478
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-02-08 14:07:20 +0100

    quick

--- _src/main.cpp
+++ _src/main.cpp
@@ -6,29 +6,28 @@ class U64
     u64 value;
 
   public:
-    U64(const u64 &v) : value(v) { line("constructor"); }
-    U64(U64 &&v) : value(v.value) { line("move constructor"); }
-    U64(const U64 &other) : value(other.value) { line("copy constructor"); }
-    U64 &operator=(const U64 &other)
+    U64(const u64& v) : value(v) { line("constructor"); }
+    U64(U64&& v) : value(v.value) { line("move constructor"); }
+    U64(const U64& other) : value(other.value) { line("copy constructor"); }
+    U64& operator=(const U64& other)
     {
         line("operator=");
         value = other.value;
         return *this;
     }
 
-    U64 operator+(const U64 &other)
+    U64 operator+(const U64& other)
     {
         line("operator+");
         return value + other.value;
     }
-
-    U64 &operator+=(const U64 &other)
+    U64& operator+=(const U64& other)
     {
         line("operator+=");
         value += other.value;
         return *this;
     }
-    U64 &operator++()
+    U64& operator++()
     {
         line("operator++ pre");
         value++;
@@ -42,19 +41,18 @@ class U64
         return previous_value;
     }
 
-    U64 operator-(const U64 &other)
+    U64 operator-(const U64& other)
     {
         line("operator-");
         return value - other.value;
     }
-
-    U64 &operator-=(const U64 &other)
+    U64& operator-=(const U64& other)
     {
         line("operator-=");
         value -= other.value;
         return *this;
     }
-    U64 &operator--()
+    U64& operator--()
     {
         line("operator-- pre");
         value--;
@@ -68,7 +66,43 @@ class U64
         return previous_value;
     }
 
-    friend std::ostream &operator<<(std::ostream &stream, const U64 &obj)
+    U64 operator*(const U64& other)
+    {
+        line("operator*");
+        return value * other.value;
+    }
+    U64& operator*=(const U64& other)
+    {
+        line("operator*=");
+        value *= other.value;
+        return *this;
+    }
+
+    U64 operator/(const U64& other)
+    {
+        line("operator/");
+        return value / other.value;
+    }
+    U64& operator/=(const U64& other)
+    {
+        line("operator/=");
+        value /= other.value;
+        return *this;
+    }
+
+    U64 operator%(const U64& other)
+    {
+        line("operator%");
+        return value % other.value;
+    }
+    U64& operator%=(const U64& other)
+    {
+        line("operator%=");
+        value %= other.value;
+        return *this;
+    }
+
+    friend std::ostream& operator<<(std::ostream& stream, const U64& obj)
     {
         // line("stream");
         stream << obj.value;
@@ -78,7 +112,7 @@ class U64
 
 // #define u64 U64
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     UTILS::clear_terminal();
     time_stamp("It just works");
