ea56a254fc8e8aa4008a6bf57a07c6488314173b
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-02-08 21:40:33 +0100

    quick

--- _src/main.cpp
+++ _src/main.cpp
@@ -1,136 +1,240 @@
 #include "__preprocessor__.h"
 #include "time_stamp.h"
 
-class U64
+#define OPERATION_COUNTER
+#define OPERATION_COUNTER_SHOW_LOG
+
+#ifdef OPERATION_COUNTER_SHOW_LOG
+#define OP_C_SHOW_LOG_LINE(...) __VA_ARGS__
+#else
+#define OP_C_SHOW_LOG_LINE(...)
+#endif
+
+template <typename T> class Operation_Counter
 {
-    u64 value;
+    T value;
 
   public:
-    U64(const u64& v) : value(v) { line("constructor"); }
-    U64(U64&& v) : value(v.value) { line("move constructor"); }
-    U64(const U64& other) : value(other.value) { line("copy constructor"); }
-    U64& operator=(const U64& other)
+    static uint64_t counter_add;
+    static uint64_t counter_sub;
+    static uint64_t counter_multi;
+    static uint64_t counter_dev;
+    static uint64_t counter_mod;
+
+    Operation_Counter(const T& v) : value(v)
     {
-        line("operator=");
+        OP_C_SHOW_LOG_LINE(line("constructor"));
+    }
+    Operation_Counter(Operation_Counter&& v) : value(v.value)
+    {
+        OP_C_SHOW_LOG_LINE(line("move constructor"));
+    }
+    Operation_Counter(const Operation_Counter& other) : value(other.value)
+    {
+        OP_C_SHOW_LOG_LINE(line("copy constructor"));
+    }
+    Operation_Counter& operator=(const T& other_value)
+    {
+        OP_C_SHOW_LOG_LINE(line("operator="));
+        value = other_value;
+        return *this;
+    }
+    Operation_Counter& operator=(const Operation_Counter& other)
+    {
+        OP_C_SHOW_LOG_LINE(line("operator="));
         value = other.value;
         return *this;
     }
 
-    U64 operator+(const U64& other)
+    Operation_Counter operator+(const Operation_Counter& other)
     {
-        line("operator+");
+        OP_C_SHOW_LOG_LINE(line("operator+"));
         return value + other.value;
     }
-    U64& operator+=(const U64& other)
+    Operation_Counter& operator+=(const Operation_Counter& other)
     {
-        line("operator+=");
+        OP_C_SHOW_LOG_LINE(line("operator+="));
         value += other.value;
         return *this;
     }
-    U64& operator++()
+    Operation_Counter operator+(const T& other_value)
     {
-        line("operator++ pre");
+        OP_C_SHOW_LOG_LINE(line("operator+"));
+        return value + other_value;
+    }
+    Operation_Counter& operator+=(const T& other_value)
+    {
+        OP_C_SHOW_LOG_LINE(line("operator+="));
+        value += other_value;
+        return *this;
+    }
+    Operation_Counter& operator++()
+    {
+        OP_C_SHOW_LOG_LINE(line("operator++ pre"));
         value++;
         return *this;
     }
-    U64 operator++(int)
+    Operation_Counter operator++(int)
     {
-        line("operator++ post");
-        U64 previous_value = *this;
+        OP_C_SHOW_LOG_LINE(line("operator++ post"));
+        Operation_Counter previous_value = *this;
         value++;
         return previous_value;
     }
 
-    U64 operator-(const U64& other)
+    Operation_Counter operator-(const Operation_Counter& other)
     {
-        line("operator-");
+        OP_C_SHOW_LOG_LINE(line("operator-"));
         return value - other.value;
     }
-    U64& operator-=(const U64& other)
+    Operation_Counter& operator-=(const Operation_Counter& other)
     {
-        line("operator-=");
+        OP_C_SHOW_LOG_LINE(line("operator-="));
         value -= other.value;
         return *this;
     }
-    U64& operator--()
+    Operation_Counter operator-(const T& other_value)
     {
-        line("operator-- pre");
+        OP_C_SHOW_LOG_LINE(line("operator-"));
+        return value - other_value;
+    }
+    Operation_Counter& operator-=(const T& other_value)
+    {
+        OP_C_SHOW_LOG_LINE(line("operator-="));
+        value -= other_value;
+        return *this;
+    }
+    Operation_Counter& operator--()
+    {
+        OP_C_SHOW_LOG_LINE(line("operator-- pre"));
         value--;
         return *this;
     }
-    U64 operator--(int)
+    Operation_Counter operator--(int)
     {
-        line("operator-- post");
-        U64 previous_value = *this;
+        OP_C_SHOW_LOG_LINE(line("operator-- post"));
+        Operation_Counter previous_value = *this;
         value--;
         return previous_value;
     }
 
-    U64 operator*(const U64& other)
+    Operation_Counter operator*(const Operation_Counter& other)
     {
-        line("operator*");
+        OP_C_SHOW_LOG_LINE(line("operator*"));
         return value * other.value;
     }
-    U64& operator*=(const U64& other)
+    Operation_Counter& operator*=(const Operation_Counter& other)
     {
-        line("operator*=");
+        OP_C_SHOW_LOG_LINE(line("operator*="));
         value *= other.value;
         return *this;
     }
-
-    U64 operator/(const U64& other)
+    Operation_Counter operator*(const T& other_value)
     {
-        line("operator/");
+        OP_C_SHOW_LOG_LINE(line("operator*"));
+        return value * other_value;
+    }
+    Operation_Counter& operator*=(const T& other_value)
+    {
+        OP_C_SHOW_LOG_LINE(line("operator*="));
+        value *= other_value;
+        return *this;
+    }
+
+    Operation_Counter operator/(const Operation_Counter& other)
+    {
+        OP_C_SHOW_LOG_LINE(line("operator/"));
         return value / other.value;
     }
-    U64& operator/=(const U64& other)
+    Operation_Counter& operator/=(const Operation_Counter& other)
     {
-        line("operator/=");
+        OP_C_SHOW_LOG_LINE(line("operator/="));
         value /= other.value;
         return *this;
     }
-
-    U64 operator%(const U64& other)
+    Operation_Counter operator/(const T& other_value)
     {
-        line("operator%");
-        return value % other.value;
+        OP_C_SHOW_LOG_LINE(line("operator/"));
+        return value / other_value;
     }
-    U64& operator%=(const U64& other)
+    Operation_Counter& operator/=(const T& other_value)
     {
-        line("operator%=");
-        value %= other.value;
+        OP_C_SHOW_LOG_LINE(line("operator/="));
+        value /= other_value;
         return *this;
     }
 
-    friend std::ostream& operator<<(std::ostream& stream, const U64& obj)
+    Operation_Counter operator%(const Operation_Counter& other)
     {
-        // line("stream");
+        OP_C_SHOW_LOG_LINE(line("operator%"));
+        return value % other.value;
+    }
+    Operation_Counter& operator%=(const Operation_Counter& other)
+    {
+        OP_C_SHOW_LOG_LINE(line("operator%="));
+        value %= other.value;
+        return *this;
+    }
+    Operation_Counter operator%(const T& other_value)
+    {
+        OP_C_SHOW_LOG_LINE(line("operator%"));
+        return value % other_value;
+    }
+    Operation_Counter& operator%=(const T& other_value)
+    {
+        OP_C_SHOW_LOG_LINE(line("operator%="));
+        value %= other_value;
+        return *this;
+    }
+
+    friend std::ostream& operator<<(std::ostream& stream,
+                                    const Operation_Counter& obj)
+    {
+        // line("stream"));
         stream << obj.value;
         return stream;
     }
 };
 
-#define u64 U64
+// #define u64 Operation_Counter<u64>
+// #define float Operation_Counter<float>
 
 int main(int argc, char* argv[])
 {
     UTILS::clear_terminal();
     time_stamp("It just works");
 
-    u64 a = 100;
-    u64 b = 10;
-    b = 100;
-    b += 1;
-    var(b);
+    if (false)
+    {
+        u64 a = 100;
+        u64 b = 10;
+        b = 100;
+        b += 1;
+        var(b);
 
-    var(++b);
-    var(b++);
-    var(b + 10);
+        var(++b);
+        var(b++);
+        var(b + 10);
 
-    var(b);
+        var(b);
 
-    a += b;
-    var(a);
+        a += b;
+        var(a);
+    }
+
+    if (true)
+    {
+        OP_C_SHOW_LOG_LINE(float a = 10.f);
+        var(a);
+        a += 10.f;
+        var(a);
+        a -= 5.f;
+        var(a);
+        a /= 2.f;
+        var(a);
+        a *= 18.f;
+        var(a);
+    }
 
     return 0;
 }
