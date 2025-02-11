4abc47fa6e9d0cd0a3b3b2b7a407ee9ad7c4a43c
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-02-11 20:03:46 +0100

    quick

--- _inc/__time_stamp__.h
+++ _inc/__time_stamp__.h
@@ -1,7 +1,6 @@
 #include "__preprocessor__.h"
 
-std::chrono::steady_clock::time_point time_stamp_last_time =
-    std::chrono::steady_clock::now();
+std::chrono::steady_clock::time_point time_stamp_last_time = std::chrono::steady_clock::now();
 
 std::string get_current_local_time()
 {
@@ -17,34 +16,59 @@ std::string get_current_local_time()
 std::string calculate_time_difference()
 {
     auto now = std::chrono::steady_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
-        now - time_stamp_last_time);
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(now - time_stamp_last_time);
     time_stamp_last_time = now; // Aktualizuj czas ostatniego pomiaru
 
     // Konwersja na jednostki czasu
-    long long milliseconds = duration.count();
-    long long hours = milliseconds / (1000 * 60 * 60);
-    milliseconds %= (1000 * 60 * 60);
-    long long minutes = milliseconds / (1000 * 60);
-    milliseconds %= (1000 * 60);
-    long long seconds = milliseconds / 1000;
-    milliseconds %= 1000;
+    long long nanoseconds = duration.count();
+    long long hours = nanoseconds / (1000LL * 1000 * 1000 * 60 * 60);
+    nanoseconds %= (1000LL * 1000 * 1000 * 60 * 60);
+    long long minutes = nanoseconds / (1000LL * 1000 * 1000 * 60);
+    nanoseconds %= (1000LL * 1000 * 1000 * 60);
+    long long seconds = nanoseconds / (1000LL * 1000 * 1000);
+    nanoseconds %= (1000LL * 1000 * 1000);
+    long long milliseconds = nanoseconds / (1000LL * 1000);
+    nanoseconds %= (1000LL * 1000);
+    long long microseconds = nanoseconds / 1000LL;
+    nanoseconds %= 1000LL;
 
-    // Formatowanie wyniku
+    bool show_all_other = false;
     std::ostringstream oss;
     if (hours > 0)
+    {
+        show_all_other = true;
         oss << hours << "h ";
-    if (minutes > 0 || hours > 0)
+    }
+    if (minutes > 0 || show_all_other)
+    {
+        show_all_other = true;
         oss << minutes << "m ";
-    if (seconds > 0 || minutes > 0 || hours > 0)
+    }
+    if (seconds > 0 || show_all_other)
+    {
+        show_all_other = true;
         oss << seconds << "s ";
-    oss << milliseconds << "ms";
+    }
+    if (milliseconds > 0 || show_all_other)
+    {
+        show_all_other = true;
+        oss << milliseconds << "mili ";
+    }
+    if (microseconds > 0 || show_all_other)
+    {
+        show_all_other = true;
+        oss << microseconds << "micro ";
+    }
+    if (nanoseconds > 0 || show_all_other)
+    {
+        show_all_other = true;
+        oss << nanoseconds << "nano ";
+    }
     return oss.str();
 }
 
-#define time_stamp(x)                                                          \
-    std::cout << "\nTIME_STAMP: " << setw(50) << left << x << right << " "     \
-              << get_current_local_time() << " (+ "                            \
+#define time_stamp(x)                                                                                                  \
+    std::cout << "\nTIME_STAMP: " << setw(50) << left << x << right << " " << get_current_local_time() << " (+ "       \
               << calculate_time_difference() << ")\n";
 #define time_stamp_reset() calculate_time_difference();
 // #define time_stamp(x)
