c7146c8a582faeec19ccb231e6efa2577fcfeae3
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-02-22 21:51:09 +0100

    quick

--- _src/main.cpp
+++ _src/main.cpp
@@ -1,6 +1,11 @@
 #include "__preprocessor__.h"
 #include "__time_stamp__.h"
 
+class test
+{
+    int tab[100];
+};
+
 #ifdef BUILD_EXECUTABLE
 int main(int argc, char* argv[])
 {
@@ -10,6 +15,9 @@ int main(int argc, char* argv[])
 
     CORE::str::split_string("Hello, World!", ' ');
     var(CORE::str::to_lower_case("Hello, World!"));
+
+    show_sizeof(test);
+    show_sizeof_many(test, 100);
 
     return 0;
 }
