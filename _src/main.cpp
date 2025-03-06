16e8e7edb4f8f33afb97ce03b1f2fcfd6baa659d
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-03-06 01:17:57 +0100

    quick

--- _src/main.cpp
+++ _src/main.cpp
@@ -1,6 +1,64 @@
 #include "__preprocessor__.h"
 #include "__time_stamp__.h"
 
+#include <cmath>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+template <typename T>
+std::string format_number(T number)
+{
+    std::string str = std::to_string(number);
+
+    // Sprawdzenie, czy liczba jest zmiennoprzecinkowa
+    size_t dot_pos = str.find('.');
+    if (dot_pos != std::string::npos)
+    {
+        // Oddziel część przed kropką
+        std::string integer_part = str.substr(0, dot_pos);
+        std::string fractional_part = str.substr(dot_pos + 1);
+
+        // Formatowanie części całkowitej
+        std::string formatted_integer;
+        int count = 0;
+        for (int i = integer_part.length() - 1; i >= 0; --i)
+        {
+            formatted_integer.insert(formatted_integer.begin(), integer_part[i]);
+            count++;
+            if (count == 3 && i != 0)
+            {
+                formatted_integer.insert(formatted_integer.begin(), '`');
+                count = 0;
+            }
+        }
+
+        // Złożenie wyniku
+        return formatted_integer + "." + fractional_part;
+    }
+    else
+    {
+        // Jeśli liczba jest całkowita
+        std::string integer_part = str;
+
+        // Formatowanie części całkowitej
+        std::string formatted_integer;
+        int count = 0;
+        for (int i = integer_part.length() - 1; i >= 0; --i)
+        {
+            formatted_integer.insert(formatted_integer.begin(), integer_part[i]);
+            count++;
+            if (count == 3 && i != 0)
+            {
+                formatted_integer.insert(formatted_integer.begin(), '`');
+                count = 0;
+            }
+        }
+
+        return formatted_integer;
+    }
+}
+
 class test
 {
     int tab[100];
@@ -18,6 +76,12 @@ int main(int argc, char* argv[])
 
     show_sizeof(test);
     show_sizeof_many(test, 100);
+
+    int num = 123456789;
+    double num2 = 1234567.89123;
+
+    std::cout << "Formatted integer: " << format_number(num) << std::endl;
+    std::cout << "Formatted float: " << format_number(num2) << std::endl;
 
     return 0;
 }
