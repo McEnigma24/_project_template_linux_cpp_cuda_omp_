7cfe70131d612d9b91771ed3caf66daf50ed41bf
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-03-08 19:52:32 +0100

    quick

--- _src/main.cpp
+++ _src/main.cpp
@@ -2,44 +2,28 @@
 
 class test
 {
-    int tab[100];
+  int tab[100];
 };
 
 #ifdef BUILD_EXECUTABLE
 int main(int argc, char* argv[])
 {
-    srand(time(NULL));
-    // CORE::clear_terminal();
-    time_stamp("It just works");
+  srand(time(NULL));
+  // CORE::clear_terminal();
+  time_stamp("It just works");
 
-    CORE::str::split_string("H"
-                            "e"
-                            "l"
-                            "l"
-                            "o"
-                            ","
-                            " "
-                            "W"
-                            "o"
-                            "r"
-                            "l"
-                            "d"
-                            "!",
-                            ' ');
-    var(CORE::str::to_lower_case("Hello"
-                                 ", "
-                                 "World"
-                                 "!"));
+  CORE::str::split_string("Hello, World!");
+  var(CORE::str::to_lower_case("Hello, World!"));
 
-    show_sizeof(test);
-    show_sizeof_many(test, 100);
+  show_sizeof(test);
+  show_sizeof_many(test, 100);
 
-    int num = 123456789;
-    double num2 = 1234567.89123;
+  int num = 123456789;
+  double num2 = 1234567.89123;
 
-    cout << CORE::format_number(num) << endl;
-    cout << CORE::format_number(num2) << endl;
+  cout << CORE::format_number(num) << endl;
+  cout << CORE::format_number(num2) << endl;
 
-    return 0;
+  return 0;
 }
 #endif
