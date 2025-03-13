09b822f9d6b3ccecd01e5ae9984b45497bfea6c1
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-03-13 18:06:50 +0100

    quick

--- _test/test.cpp
+++ _test/test.cpp
@@ -1,11 +1,9 @@
 // clang-format off
 #include <gtest/gtest.h> // must be first
+// clang-format on
 #include "__preprocessor__.h"
 
-TEST(SampleTest, Sanity)
-{
-    EXPECT_EQ(1, 1);
-}
+TEST(SampleTest, Sanity) { EXPECT_EQ(1, 1); }
 
 int main(int argc, char** argv)
 {
