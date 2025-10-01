b72db8f5df2f332aabbb6f40328a3b59c3a22673
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-10-01 23:14:47 +0200

    quick

--- _inc/parallel_common.h
+++ _inc/parallel_common.h
@@ -1,5 +1,17 @@
 #pragma once
 
+#ifdef GPU
+#define GPU_LINE(...) __VA_ARGS__
+#else
+#define GPU_LINE(...) 
+#endif
+
+#ifdef CPU
+#define CPU_LINE(...) __VA_ARGS__
+#else
+#define CPU_LINE(...) 
+#endif
+
 #ifdef CPU
 #include <omp.h>
 #endif
